7558ef67a9c3be142dd5fcb93f855d3705c68f1b
Sagnik Chandra <sagnikc2003@gmail.com>
2026-06-09 13:40:47 +0530

    added data

--- attention_sink_analysis/attention_sink_computation.py
+++ attention_sink_analysis/attention_sink_computation.py
@@ -3,9 +3,10 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 def measure_attention_sink(model):
     
-    num_layers=model.config.num_hidden_layers
+    num_layers=model.config.num_hidden_layers     # Number of layers in the model
+    num_heads=model.config.num_attention_heads    # Number of attention heads per layer
 
-    return num_layers
+    return num_heads, num_layers
 
 
 model=AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3.1-8B-Instruct")
